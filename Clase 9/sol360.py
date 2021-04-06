b1dc8bc3fba70aa861af0d872488d70d7e957ad1
GodzillaGonzo <gonzalodr@eant.tech>
2021-04-05 22:04:24 -0300

    Se agrega log de errores para ciudades no encontradas

--- Clase 9/sol360.py
+++ Clase 9/sol360.py
@@ -5,13 +5,18 @@ import json
 key = 'f6aebc1f999ab9b6a8685e0d60bd5373'
 
 archivo_in = open('sucursales_sol_360.csv')
+log_error = open('sol360_log_error.txt', 'w')
 
 for linea in archivo_in:
     linea = linea.split(';')
-    print(linea[0])
     ciudad_cod = requests.utils.quote(linea[0])
     provincia_cod = requests.utils.quote(linea[1])
     url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ciudad_cod + ',Argentina&lang=es&appid=' + key
     objeto = json.loads(requests.get(url).text)
-    descripcion = objeto.get('weather')[0]['description']
-    
+    if objeto.get('weather') == None:
+        log_error.write(linea[0] + " no encontrada\n")
+    else:
+        print(linea[0] , '->', objeto.get('weather')[0]['description'])
+
+
+log_error.close()
