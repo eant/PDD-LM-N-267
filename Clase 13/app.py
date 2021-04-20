9c3717c908e61b16da008bf4894f914514d9496b
smessina <info@silviomessina.com>
2021-04-20 11:08:19 -0300

    Conectando Flask a MongoDB Atlas con env vars

--- Clase 13/app.py
+++ Clase 13/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, json
+from pymongo import MongoClient
+import settings
+from os import environ
 
 app = Flask(__name__)
+
+USER = environ['USRM']
+PASS = environ['PASS']
+HOST = environ['HOST']
+BASE = environ['BASE']
+
+client = MongoClient("mongodb+srv://"+USER+":"+PASS+"@"+HOST+"/"+BASE+"?retryWrites=true&w=majority")
 
 @app.route('/')
 def hello_flask():
@@ -18,6 +28,7 @@ def usersTwitter():
     response = app.response_class( response = json.dumps(users), status = 200, mimetype = 'application/json' )
 
     return response
+
 ###################
 @app.route('/users/<path>')
 def searchTwitter(path):
@@ -30,6 +41,20 @@ def searchTwitter(path):
     
     return response
 
+###################
+@app.route('/api/tweets')
+def getTweets():
+
+    bigdata = client['bigdata']
+    tweets = bigdata['tweets']
+
+    los_tweets = tweets.find()
+
+    for tweet in los_tweets:
+
+        print( tweet['id_str'] )
+
+    return '{ "rta" : "ok" }'
 
 if __name__ == '__main__':
     app.run( port = 3030, host = "0.0.0.0" )
